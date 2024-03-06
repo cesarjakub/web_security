79a7c6c34741245cff47fb87d9129ac5b0374a12
cesarjakub <cesar@spsejecna.cz>
2024-03-06 20:50:43 +0100

    feat: register style done

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def login():
     return render_template("login.html")
 
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
